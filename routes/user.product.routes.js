1ca15358fa2c53b450a5afbe170748c9f6619136
Kouvousis <dexous1996@gmail.com>
2024-09-27 21:02:52 +0300

    small change to patch path

--- routes/user.product.routes.js
+++ routes/user.product.routes.js
@@ -5,7 +5,7 @@ const userProductController = require("../controllers/user.product.controller");
 router.get("/users/products", userProductController.findAll);
 router.get("/:username/products", userProductController.findOne);
 router.post("/:username/products", userProductController.create);
-router.patch("/:username", userProductController.update);
+router.patch("/:username/products/:id", userProductController.update);
 router.delete("/:username/products/:id", userProductController.delete);
 router.get("/stats1", userProductController.stats1);
 router.get("/:username/stats2", userProductController.stats2);
