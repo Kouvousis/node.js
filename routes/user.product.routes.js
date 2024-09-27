1dacf4476b75324bb3cb4d3a7d50db6685dccf5c
Kouvousis <dexous1996@gmail.com>
2024-09-27 21:00:41 +0300

    finished user.product.controller and routes

--- routes/user.product.routes.js
+++ routes/user.product.routes.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const userProductController = require("../controllers/user.product.controller");
 
-router.get("/", userProductController.findAll);
-router.get("/:username", userProductController.findOne);
-router.post("/", userProductController.create);
+router.get("/users/products", userProductController.findAll);
+router.get("/:username/products", userProductController.findOne);
+router.post("/:username/products", userProductController.create);
 router.patch("/:username", userProductController.update);
 router.delete("/:username/products/:id", userProductController.delete);
+router.get("/stats1", userProductController.stats1);
+router.get("/:username/stats2", userProductController.stats2);
 
 module.exports = router;
---
+++ swagger.js
@@ -0,0 +1,272 @@
+const m2s = require("mongoose-to-swagger");
+const User = require("./models/user.model");
+const { patch } = require("./routes/user.routes");
+const { response } = require("express");
+
+exports.options = {
+  openapi: "3.1.0",
+  info: {
+    version: "1.0.0",
+    title: "Products CRUD API",
+    description: "Products and Users application",
+    contact: {
+      name: "Coding Factory",
+      url: "https://www.example.com",
+      email: "support@example.com",
+    },
+  },
+  components: {
+    schemas: {
+      User: m2s(User),
+    },
+  },
+  servers: [
+    {
+      url: "http://localhost:3000",
+      description: "Local Server",
+    },
+    {
+      url: "https://www.example.com",
+      description: "Testing Server",
+    },
+  ],
+  tags: [
+    {
+      name: "Users",
+      description: "Requests for user",
+    },
+    {
+      name: "Users and Products",
+      description: "Request for user products",
+    },
+  ],
+  paths: {
+    "/api/users": {
+      get: {
+        tags: ["Users"],
+        description: "Returns all users",
+        responses: {
+          200: {
+            description: "List of all users",
+            content: {
+              "application/json": {
+                // MIME type corrected to lowercase
+                schema: {
+                  type: "array",
+                  items: {
+                    $ref: "#/components/schemas/User", // Referencing the User schema
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+      post: {
+        tags: ["Users"],
+        description: "Creates a new user",
+        requestBody: {
+          description: "Data for user that we create",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  username: { type: "string" },
+                  password: { type: "string" },
+                  name: { type: "string" },
+                  surname: { type: "string" },
+                  email: { type: "email" },
+                  "address:": {
+                    type: "object",
+                    properties: {
+                      area: { type: "string" },
+                      road: { type: "string" },
+                    },
+                  },
+                  phone: {
+                    type: "array",
+                    items: {
+                      type: "object",
+                      properties: {
+                        type: { type: "string" },
+                        number: { type: "string" },
+                      },
+                    },
+                  },
+                },
+                required: ["username", "password", "name", "surname"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: "New user is created",
+          },
+        },
+      },
+    },
+    "/api/users/{username}": {
+      get: {
+        tags: ["Users"],
+        parameters: [
+          {
+            name: "username",
+            in: "path",
+            required: true,
+            description: "Username of the user that we want to find",
+            type: "string",
+          },
+        ],
+        description: "Get user with specific username",
+        responses: {
+          200: {
+            description: "User result",
+            schema: {
+              $ref: "#/components/schemas/User",
+            },
+          },
+        },
+      },
+      patch: {
+        tags: ["Users"],
+        description: "Update user",
+        parameters: [
+          {
+            name: "username",
+            in: "path",
+            required: true,
+            description: "Username of user that we want to update",
+            type: "string",
+          },
+        ],
+        requestBody: {
+          description: "User to update",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  name: { type: "string" },
+                  surname: { type: "string" },
+                  email: { type: "string" },
+                  address: {
+                    type: "object",
+                    properties: {
+                      area: "string",
+                      road: "string",
+                    },
+                  },
+                },
+                required: ["email"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: "Update user",
+            schema: {
+              $ref: "#components/schema/User",
+            },
+          },
+        },
+      },
+      delete: {
+        tags: ["Users"],
+        description: "Deletes a user",
+        parameters: [
+          {
+            name: "username",
+            in: "path",
+            required: true,
+            description: "Username of user that we want to delete",
+            type: "string",
+          },
+        ],
+        responses: {
+          200: {
+            description: "Delete a user",
+          },
+        },
+      },
+    },
+    "/api/user-product/users/products": {
+      get: {
+        tags: ["Users and Products"],
+        description: "Returns all users and their products",
+        responses: {
+          200: {
+            description: "All users with their products",
+          },
+        },
+      },
+    },
+    "/api/user-product/{username}/products": {
+      get: {
+        tags: ["Users and Products"],
+        parameters: [
+          {
+            name: "username",
+            in: "path",
+            required: true,
+            description: "Username of the user to find products",
+            type: "string",
+          },
+        ],
+        description: "Returns all the products of a user",
+        responses: {
+          200: {
+            description: "User and products to find",
+          },
+        },
+      },
+      post: {
+        tags: ["Users and Products"],
+        description: "Add new products to user",
+        parameters: [
+          {
+            name: "username",
+            in: "path",
+            required: true,
+            description: "Username of the user to find products",
+            type: "string",
+          },
+        ],
+        requestBody: {
+          description: "Data to add products",
+          content: {
+            "application/json": {
+              schema: {
+                type: "object",
+                properties: {
+                  username: { type: "string" },
+                  products: {
+                    type: "array",
+                    items: {
+                      type: "object",
+                      properties: {
+                        product: { type: "string" },
+                        cost: { type: "number" },
+                        quantity: { type: "number" },
+                      },
+                    },
+                  },
+                },
+                required: ["quantity"],
+              },
+            },
+          },
+        },
+        responses: {
+          200: {
+            description: "New products added to user",
+          },
+        },
+      },
+    },
+  },
+};
+
+module.exports = exports.options;
