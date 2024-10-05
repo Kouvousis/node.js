ee04aeadf0d4660ee17d1e7dfae86a0f6ae1215e
Kouvousis <dexous1996@gmail.com>
2024-10-05 19:23:00 +0300

    fixed wrong required fields

--- models/product.model.js
+++ models/product.model.js
@@ -1,32 +1,33 @@
-const mongoose = require('mongoose')
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-let productSchema = new Schema({
+let productSchema = new Schema(
+  {
     product: {
-        type: String,
-        required: [true, "Product name is required"],
-        max: 100,
-        lowercase: true,
-        trim: true
+      type: String,
+      required: [true, "Product name is required"],
+      max: 100,
+      lowercase: true,
+      trim: true,
     },
     cost: {
-        type: Number
+      type: Number,
+      require: true,
     },
     description: {
-        type: String,
-        required: [true, "Product description is required"],
-        max: 100,
-        lowercase: true
+      type: String,
+      max: 100,
+      lowercase: true,
     },
     quantity: {
-        type: Number,
-        required: true
-    }
-},
-{
-    collection: 'products',
-    timestamps: true
-}
-)
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    collection: "products",
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('Product', productSchema)
+module.exports = mongoose.model("Product", productSchema);
