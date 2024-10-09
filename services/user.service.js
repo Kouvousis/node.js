14fba76a77d037d73a25623d72eff3b39664c152
Kouvousis <dexous1996@gmail.com>
2024-10-09 21:06:22 +0300

    user.product.test created

--- services/user.service.js
+++ services/user.service.js
@@ -11,4 +11,37 @@ async function findLastInsertedUser() {
   }
 }
 
-module.exports = { findLastInsertedUser };
+// async function findUsersProduct(username, _id) {
+//   console.log("Find user's product", username, _id);
+
+//   try {
+//     const result = User.findOne(
+//       {
+//         username: username,
+//         "products._id": _id,
+//       },
+//       { username: 1, "products.$": 1 }
+//     );
+//     return result;
+//   } catch (err) {
+//     console.log("Problem in finding user's product");
+//     return false;
+//   }
+// }
+
+async function findUsersProduct(username) {
+  console.log("Find user's product", username);
+
+  try {
+    const result = User.findOne(
+      { username: username },
+      { username: 1, products: 1 }
+    );
+    return result;
+  } catch (err) {
+    console.log("Problem in finding user's product");
+    return false;
+  }
+}
+
+module.exports = { findLastInsertedUser, findUsersProduct };
